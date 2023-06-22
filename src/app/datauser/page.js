c25ee2de48a3381762f08bb768d64c133e5c62fd
kansinee02 <137372501+kansinee02@users.noreply.github.com>
2023-06-22 11:55:38 +0700

    ออกระบบได้เลย
    ออกระบบได้เลย

--- src/app/datauser/page.js
+++ src/app/datauser/page.js
@@ -64,7 +64,7 @@ export default function Home() {
               fontFamily: "Noto Sans Thai",
             }}
           >
-            ออกจากระบบ
+            ออกจากระบบได้เลย
           </Typography></Link>
         </Box>
 
